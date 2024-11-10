0293719fbe12a7f734de4f421d0464e33013360f
Waqar <waqar-1@hotmail.co.uk>
2024-11-10 04:28:16 +0000

    chore: project setup frontend using vite & backend using spring initializer

---
+++ backend/src/main/java/com/w_farooq_group/lidlmanagment/LidlmanagmentApplication.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group.lidlmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LidlmanagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LidlmanagmentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/w_farooq_group/lidlmanagment/LidlmanagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group.lidlmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LidlmanagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
