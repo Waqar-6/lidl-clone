9f4c86ef22dd9e49c63f6455810d3d9b52db6e77
Waqar <waqar-1@hotmail.co.uk>
2024-11-10 21:05:48 +0000

    feat(auth): configure Spring Security for basic authentication and form login
    - Added \SecurityFilterChain\ bean to permit all requests, configure form login, and enable basic authentication.
    - Added \AuthenticationManager\ bean for managing authentication.
    - Added \PasswordEncoder\ bean with BCrypt encoding for password encryption.

---
+++ backend/src/main/java/com/w_farooq_group/lidlmanagment/auth/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.w_farooq_group.lidlmanagment.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder () {return new BCryptPasswordEncoder();}
+
+}
